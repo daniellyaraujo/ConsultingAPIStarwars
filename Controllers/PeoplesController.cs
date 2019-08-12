2373796d8918ccaeaafda8bbaef8c83e3b789069
Araujo <Danielly.Araujo@capco.com>
2019-08-12 14:15:13 -0300

    Add project files.

---
+++ Controllers/PeoplesController.cs
@@ -0,0 +1,62 @@
+﻿using ConstruindoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+namespace ConstruindoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public partial class PeoplesController : ControllerBase
+    {
+        private string _host = "https://swapi.co/api";
+        private readonly HttpClient _client;
+
+        public PeoplesController()
+        {
+            _client = new HttpClient();
+        }
+
+        // GET api/values
+        //[HttpGet("{id}")]
+        //public ActionResult GetPeopleById(int id)
+        //{
+        //    string url = $"{ _host}/people/{ id}";
+
+        //    var result = _client.GetAsync(url).Result;
+        //    var pessoa = result.Content.ReadAsAsync<Pessoas>().Result;
+
+        //    return new OkObjectResult(pessoa);
+        //}
+
+        [HttpGet]
+        public ActionResult GetPeopleAll()
+        {
+            string url = $"{_host}/people";
+
+            var result = _client.GetAsync(url).Result;
+            var listaPessoas = result.Content.ReadAsAsync<ListPessoas>().Result;
+
+            var OrdenedbyFilms = new List<Pessoas>();
+            var soma = 0;
+            var qtdPessoas = 0;
+            
+            foreach (var pessoa in listaPessoas.Results)
+            {
+                var pesoDaPessoa = pessoa.Mass;
+
+                var especieDaPessoa = pessoa.Species.ToList()[0];
+
+                if (string.Equals(especieDaPessoa, "https://swapi.co/api/species/1/"))
+                {
+                    soma = pesoDaPessoa + soma;
+                    qtdPessoas = qtdPessoas + 1;
+                }
+            }
+            var mediaPeso = soma / qtdPessoas;
+
+            return new OkObjectResult(mediaPeso);
+        }
+    }
+}
---
+++ Models/Pessoas.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConstruindoAPI.Models
+{
+    public class ListPessoas
+    {
+        public IList<Pessoas> Results { get; set; }
+    }
+
+    public class Pessoas
+    {
+        public string Name { get; set; }
+        public int Height { get; set; }
+        public int Mass { get; set; }
+        public string Skin_Color { get; set; }
+        public string Hair_Color { get; set; }
+        public string Eye_Color { get; set; }
+        public string Birth_Year { get; set; }
+        public string Gender { get; set; }
+        public string HomeWorld { get; set; }
+        public ICollection<string> Films { get; set; }
+        public ICollection<string> Species { get; set; }
+        public ICollection<string> Vehicles { get; set; }
+        public ICollection<string> Starships { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+        public string Url { get; set; }
+
+
+    }
+}
